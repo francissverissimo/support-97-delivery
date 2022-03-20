ec6f666236da51aa324e3db77ff6878adf592828
Francis Verissimo <francissv97@gmail.com>
2022-03-20 17:00:09 -0400

    adjust div#rate, use usePopularArticlesContext

--- src/pages/Article.tsx
+++ src/pages/Article.tsx
@@ -1,26 +1,28 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { Markup } from "interweave";
 
 import { firebase, database } from "../services/firebase";
+import { useArticle } from "../hooks/useArticle";
+import { usePopularArticlesContext } from "../hooks/usePopularArticlesContext";
 
 import { Footer } from "../components/Footer";
 import { Header } from "../components/Header";
 
 import "../styles/article.scss";
-import { useArticle } from "../hooks/useArticle";
-
+  
 type ArticleParams = {
-  doc: string;
+  id: string;
 };
 
 export function Article() {
+  const { popularArticles } = usePopularArticlesContext();
+  const navigate = useNavigate();
   const params = useParams() as ArticleParams;
-  const doc = params.doc;
-
-  const article = useArticle(doc);
+  const id = params.id;
+  const { article } = useArticle(id);
 
   async function sortArticleWithUseful() {
-    const docRef = database.collection("articles").doc(doc);
+    const docRef = database.collection("articles").doc(id);
 
     await docRef.update({
       "rate.useful": firebase.firestore.FieldValue.increment(1)
@@ -28,11 +30,15 @@ export function Article() {
   }
 
   async function sortArticleWithUseless() {
-    const docRef = database.collection("articles").doc(doc);
+    const docRef = database.collection("articles").doc(id);
 
     await docRef.update({
       "rate.useless": firebase.firestore.FieldValue.increment(1)
     });
+  }
+
+  async function browseAmongRelated(text: string) {
+    navigate(`/article/${text}`);
   }
 
   return (
@@ -54,38 +60,46 @@ export function Article() {
         <div id="content-article">
           <h2 className="title">{article?.title}</h2>
           <Markup content={article?.body} />
+          <div id="rate">
+            <h4>Isso foi útil?</h4>
+            <div id="rateButtons">
+              <i
+                id="useful"
+                className="fa-solid fa-thumbs-up"
+                onClick={sortArticleWithUseful}
+              ></i>
+              <i
+                id="useless"
+                className="fa-solid fa-thumbs-down"
+                onClick={sortArticleWithUseless}
+              ></i>
+            </div>
+          </div>
         </div>
         <aside id="sidebar">
           <h3 className="title">Populares</h3>
           <ul>
-            <li>
-              <a href="#">Primeiros passos no programa?</a>
-            </li>
-            <li>
-              <a href="#">
-                Como criar uma chave para utilizar os mapas do Google?
-              </a>
-            </li>
-            <li>
-              <a href="#">Como configurar um IP fixo no Computador Servidor?</a>
-            </li>
-            <li>
-              <a href="#">
-                Como configurar a versão rede entre vários computadores?
-              </a>
-            </li>
-            <li>
-              <a href="#">Como Integrar o Programa ao iFood?</a>
-            </li>
+            {popularArticles?.map(e => {
+              return (
+                <li key={e.id}>
+                  <a onClick={() => browseAmongRelated(e.id)}>{e.title}</a>
+                </li>
+              );
+            })}
+          </ul>
+          <h3 className="title">Relacionados</h3>
+          <ul>
+            {article?.related.map(relatedArticle => {
+              return (
+                <li key={relatedArticle.id}>
+                  <a onClick={() => browseAmongRelated(relatedArticle.id)}>
+                    {relatedArticle.title}
+                  </a>
+                </li>
+              );
+            })}
           </ul>
         </aside>
-      </div>
-      <div id="rate">
-        <h4>Isso foi útil?</h4>
-        <div id="rateButtons">
-          <i id="useful" className="fa-solid fa-thumbs-up" onClick={sortArticleWithUseful}></i>
-          <i id="useless" className="fa-solid fa-thumbs-down" onClick={sortArticleWithUseless}></i>
-        </div>
       </div>
       <Footer />
     </div>
