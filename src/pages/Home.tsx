438c6b55d5db05798606fdccc6f2de3f4fba7786
Francis Verissimo <francissv97@gmail.com>
2022-03-22 14:12:32 -0400

    update cards

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -29,9 +29,21 @@ export function Home() {
             <h2>Iniciando com Zap Delivery</h2>
           </div>
           <div className="cards">
-            <Card iconClass="fa fa-user-plus" text="Como criar minha Conta" />
-            <Card iconClass="fa fa-magic" text="Como criar meu App" />
-            <Card iconClass="fa fa-bullhorn" text="Como divulgar minha loja" />
+            <Card
+              iconClass={"fa fa-user-plus"}
+              text="Como criar minha Conta"
+              id={"como-criar-minha-conta"}
+            />
+            <Card
+              iconClass={"fa fa-magic"}
+              text="Como criar meu App"
+              id={"como-criar-meu-app"}
+            />
+            <Card
+              iconClass={"fa fa-bullhorn"}
+              text="Como divulgar minha loja"
+              id={"como-divulgar-minha-loja"}
+            />
           </div>
 
           <div id="rule-dark"></div>
@@ -40,27 +52,66 @@ export function Home() {
             <h2>Configurando o Meu Negócio</h2>
           </div>
           <div className="cards">
-            <Card iconClass="fa fa-store" text="Editar minhas informações" />
-            <Card iconClass="fa fa-brush" text="Temas e Cores" />
-            <Card iconClass="fa fa-qrcode" text="Métodos de Operação" />
             <Card
-              iconClass="fa fa-map-marker-alt"
-              text="Cadastrar Meu Enderenço"
+              iconClass={"fa fa-store"}
+              text={"Editar minhas informações"}
+              id={"editar-minhas-informacoes"}
             />
-            <Card iconClass="fa fa-comment" text="Contatos e Mídias Sociais" />
             <Card
-              iconClass="fa fa-clock-four"
-              text="Horários de Funcionamento"
+              iconClass={"fa fa-brush"}
+              text={"Temas e Cores"}
+              id={"temas-e-cores"}
             />
-            <Card iconClass="fa fa-money-bill" text="Taxas de Entrega" />
             <Card
-              iconClass="fa fa-hand-holding-dollar"
-              text="Formas de Pagamento"
+              iconClass={"fa fa-qrcode"}
+              text={"Métodos de Operação"}
+              id={"metodos-de-operacao"}
             />
-            <Card iconClass="fa fa-credit-card-alt" text="Pagamento Online" />
-            <Card iconClass="fa fa-receipt" text="Impressão de Comandas" />
-            <Card iconClass="fa fa-phone" text="Integração com WhatsApp" />
-            <Card iconClass="fa fa-cogs" text="Preferências" />
+            <Card
+              iconClass={"fa fa-map-marker-alt"}
+              text={"Cadastrar Meu Enderenço"}
+              id={"cadastrar-meu-enderenco"}
+            />
+            <Card
+              iconClass={"fa fa-comment"}
+              text={"Contatos e Mídias Sociais"}
+              id={"contatos-e-midias-sociais"}
+            />
+            <Card
+              iconClass={"fa fa-clock-four"}
+              text={"Horários de Funcionamento"}
+              id={"horarios-de-funcionamento"}
+            />
+            <Card
+              iconClass={"fa fa-money-bill"}
+              text={"Taxas de Entrega"}
+              id={"taxas-de-entrega"}
+            />
+            <Card
+              iconClass={"fa fa-hand-holding-dollar"}
+              text={"Formas de Pagamento"}
+              id={"formas-de-pagamento"}
+            />
+            <Card
+              iconClass={"fa fa-credit-card-alt"}
+              text={"Pagamento Online"}
+              id={"pagamento-online"}
+            />
+            <Card
+              iconClass={"fa fa-receipt"}
+              text={"Impressão de Comandas"}
+              id={"impressao-de-comandas"}
+            />
+            <Card
+              iconClass={"fa fa-phone"}
+              text={"Integração com WhatsApp"}
+              id={"integracao-com-whatsapp"}
+            />
+            <Card
+              iconClass={"fa fa-cogs"}
+              text={"Preferências"}
+              id={"preferencias"}
+            />
           </div>
         </div>
       </div>
