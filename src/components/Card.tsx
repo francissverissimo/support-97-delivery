998bfe99299bda40e8542c2ee7e69534f25c0813
Francis Verissimo <francissv97@gmail.com>
2022-03-22 14:03:47 -0400

    add navigation

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,13 +1,17 @@
+import { useNavigate } from "react-router-dom";
 import "../styles/card.scss";
 
 type CardProps = {
+  id: string;
   iconClass?: string;
   text: string;
-}
+};
 
 export function Card(props: CardProps) {
+  const navigate = useNavigate();
+
   return (
-    <div className="card">
+    <div className="card" onClick={() => navigate(`/article/${props.id}`)}>
       <i className={props.iconClass}></i>
       <div>{props.text}</div>
     </div>
